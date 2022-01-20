9e5a67ca81b3727a675a0ad91bda5959bc506569
Nicholas Kim <nick_kim00@hotmail.com>
2022-01-19 22:37:25 -0800

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div class="Nav-bar">
+        <a class="active" href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+      </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>Welcome to the new Zotbotics website!</h1>
