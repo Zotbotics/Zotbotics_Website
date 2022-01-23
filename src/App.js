639e06313e90615346e4d52442ec30eab0a55122
Nicholas Kim <nick_kim00@hotmail.com>
2022-01-22 23:12:51 -0800

    Added details to navbar, added placeholder text and css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,23 +6,29 @@ function App() {
     <div className="App">
       <header className="App-header">
       <div class="Nav-bar">
-        <a class="active" href="/">Home</a>
-        <a href="/contact">Contact</a>
-        <a href="/about">About</a>
+        <div class="Nav-bar-links">
+          <a class="Nav-bar-text" href="/Zotbotics_Website">Home</a>
+          <a class="Nav-bar-text" href="/about">About</a>
+          <a class="Nav-bar-text" href="/interested-students">Interested Students</a>
+          <a class="Nav-bar-text" href="/tutorials">Tutorials</a>
+          <a class="Nav-bar-text" href="/team5">Team 5</a>
+        </div>
       </div>
-        <img src={logo} className="App-logo" alt="logo" />
+      <div class="home-body">
         <p>
-          <h1>Welcome to the new Zotbotics website!</h1>
-          <p>Hellow</p>
+          <h1>Zotbotics</h1>
         </p>
-        <a
+      </div>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
