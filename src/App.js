25834e06d6e2bf4777a7be56aa782e43fe957d96
Nick <nick_kim00@hotmail.com>
2022-03-26 00:14:39 -0700

    Major html/css added to match mockup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 // import logo from "./logo.svg";
 import logo from "./logonbg.png"
 import mascot from "./mascot.png"
+import fb from "./fblogo.png"
+import ig from "./iglogo.png"
+import gh from "./ghlogo.png"
+import rock from "./dtrj.jpg"
 import "./App.css";
 
 function App() {
@@ -10,7 +14,6 @@ function App() {
       <div class="Nav-bar">
         <div class = "Nav-bar-logo">
           <img src = {mascot} id = "mascot" alt="Zotbotics mascot"></img>
-          {/* <p>Zotbotics</p> */}
         </div>
         <div class="Nav-bar-links">
           <a class="Nav-bar-text" href="/Zotbotics_Website">Home</a>
@@ -25,34 +28,93 @@ function App() {
       <div class="home-body" id = "intro">
         <h1 id = "intro-header">Zotbotics is a project based robotics club at UCI, open to all majors and experience levels.</h1>
           <p class = "home-body-text" id = "intro-sub">We are a club dedicated to exploring the field of robotics as well as building interesting and creative projects.</p>
-        <a href="something" class="button1">Learn More</a>
+        <a href="something" class="button1" id = "scroll-button">Learn More</a>
+        <style class = "dotted-line"></style>
       </div>
-      <div class="home-body" id="interested">
-        <h1>Interested Students</h1>
-        <p class = "home-body-text">For the Fall 2020 quarter, Zotbotics will be hosting workshops regarding robotics. Each workshop will consist of informational slides/videos demonstrating how a certain part is integral to robotics and how it works. For workshops more focused on software, we will also share Google Colabs where people can interact with the source code.</p>
-        <p class = "home-body-text">Interested? Please fill out this form and subscribe to our newsletter.</p>
+    
+      <div class="purpose">
+        <ul class = "purpose-left">
+          <li>
+            <img src = {rock} id = "purpose-img1" alt="Zotbotics mascot"></img>
+          </li>
+          <li>
+            <p class = "purpose-text">
+              For the Fall 2020 quarter, Zotbotics will be hosting workshops regarding robotics.
+              Each workshop will consist of informational slides/videos demonstrating
+              how a certain part is integral to robotics and how it works. For workshops more focused
+              on software, we will share Google Colabs where peopl can interact with the souce code.
+            </p>
+          </li>
+          <li>
+            <p class = "purpose-text">
+              For the Winter 2021 quarter, Zotbotics members will be able to build their own
+              fully functional robot that will be adaptable for future modifications. Teams will begin
+              with a starter kit of parts and will be able to follow tutorials created bodyotbotics 
+              for any programming, electrical designs, and mechanical designs necessary for the robot's 
+              basic functions. From there, more advanced addons can be built into the robot, making
+              the kit flexible and great for those getting into robotics, as well as 
+              those already with some experience.
+            </p>
+          </li>
+        </ul>
+
+        <div class = "purpose-center">
+
+        </div>
+
+        <ul class = "purpose-right">
+          <li>
+            <h1>OUR PURPOSE</h1>
+          </li>
+          <li>
+            <p class = "purpose-text">The purpose of Zotbotics is to provide a community where students interested in the field of robotics can work on fully functional robots and apply their skills in mechanical engineering, electornics, and computer science.</p>
+          </li>
+          <li>
+            <p class = "purpose-text">Our members gain valuable experience that is applicable to education, research, and internships/careeers.</p>
+          </li>
+          <li>
+            <img src = {rock} id = "purpose-img1" alt="Zotbotics mascot"></img>
+          </li>
+          <li>
+            <img src = {rock} id = "purpose-img1" alt="Zotbotics mascot"></img>
+          </li>
+        </ul>
       </div>
-      <div class="home-body" id ="tutorials">
-        <h1>Tutorials</h1>
-        <p class = "home-body-text">If you are interested in checking out what we'll be working on, please check out the following resources!</p>
+      <div class = "purpose" id = "purpose-bottom">
+        <img src = {mascot} id = "purpose-mascot" alt="Zotbotics mascot"></img>
       </div>
-      <div class="footer">
-      <ul>
-        <li class = "footer-link">instagram link</li>
-        <li class = "footer-link">other link</li>
-        <li class = "footer-link">newsletter signup link</li>
-      </ul>
+
+      <div class = "contact">
+        <li id = "contact-list">
+          <ul>
+            <p class = "contact-text">For inquiries, email:</p>
+            <p class = "contact-text">zotbotics@gmail.com</p>
+          </ul>
+          <ul>
+            <img src = {mascot} id = "contact-mascot" alt="Zotbotics mascot"></img>
+          </ul>
+        </li>
       </div>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
+      <div id="footer">
+        <ul id = "footer-ul">
+          <li class = "footer-link">
+            <a href="https://www.facebook.com/zotbotics">
+              <img src={fb} alt = "facebook logo" height = "25em" width = "25em"></img>
+            </a>
+          </li>
+          <li class = "footer-link">
+            <a href="https://www.instagram.com/zotbotics">
+              <img src={ig} alt = "instagram logo" height = "25em" width = "25em"></img>
+            </a>
+          </li>
+          <li class = "footer-link">
+            <a href="https://www.github.com/zotbotics">
+              <img src={gh} alt = "github logo" height = "25em" width = "25em"></img>
+            </a>
+          </li>
+        </ul>
+      </div>
       </header>
     </div>
   );
